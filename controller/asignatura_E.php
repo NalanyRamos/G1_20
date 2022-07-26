81232fcfb66008a8325e827491d8cf8bee9a38da
JaderX24 <jader.menjivar@unah.hn>
2022-07-26 16:35:43 -0600

    modified:   controller/asignatura_E.php

--- controller/asignatura_E.php
+++ controller/asignatura_E.php
@@ -12,7 +12,7 @@
     header('Access-Control-Allow-Origin: *');  
     header('Content-Type: application/json');
 
-    require_once("../config/conexionAsignatura.php");
+    require_once("../config/conexion.php");
     require_once("../models/Asignatura_E.php");
 
     $asignatura = new Asignatura();
