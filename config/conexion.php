2ff8703e2c999d9c40be3ff63108cbb281a6d712
NalanyRamos <109555896+NalanyRamos@users.noreply.github.com>
2022-07-26 13:42:31 -0600

    Codigo de conexion php

---
+++ config/conexion.php
@@ -0,0 +1,20 @@
+<?php
+    class Conectar{
+        protected $dbh;
+
+        protected function Conexion(){
+            try {
+                $conectar = $this-> dbh = new PDO("mysql:host=20.216.41.245;dbname=G1_20","G1_20","fVl9pd2E");
+                return $conectar;
+
+            }catch(Exception $e){
+                print "Error BD!: " . $e->getMessage() . "<br/>";
+                die();
+            }
+        }
+        public function set_names(){
+            return $this->dbh->query("SET NAMES 'utf8'");
+        }
+
+    }
+?>
